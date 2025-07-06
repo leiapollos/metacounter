d19cd0e83f9a12e16c5d505f60c4e5ffaa85873e
André Leite <andreleite@Andres-MacBook-Pro.local>
2025-07-06 12:26:26 +0100

    First commit

---
+++ src/game/player.cpp
@@ -0,0 +1,8 @@
+#include "player.h"
+
+// This tests the duplicate detection.
+const auto DUMMY_PLAYER_HEALTH_REG = REGISTER_COUNTER(PlayerHealth);
+
+void Player::TakeDamage(int amount) {
+    // ... logic for taking damage
+}
